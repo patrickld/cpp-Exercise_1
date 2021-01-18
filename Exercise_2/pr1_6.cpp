30002abc62e581ccc80ab564fd335cd5803b8695
patrickkk <leyendeckerpatrick@gmail.com>
2021-01-18 22:16:41 +0000

    update

---
+++ Exercise_2/pr1_6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+//counter for recursive calls
+int a=0; 
+
+int fibonacci(int n=0){
+  a++;
+  if(n==1)
+    return n-1;
+  else if(n==2)
+    return n-1;
+  else
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+  int n;
+  cout<<"Enter the wanted number of the Fibonacci sequence: ";
+  cin>>n;
+  cout<<"The "<<n<<". Fibonacci number is "<<fibonacci(n)<<endl;
+  cout<< "The number of recursive calls is: "<<a<<endl;
+  return 0;
+}
